2cc4fb0bd3b8d666d04273ae579446bdd26150d3
daoodaba975 <davinhoba9@outlook.fr>
2022-02-07 13:43:28 +0000

    update 🆗 work in progres ✅ some change 🔄 stylizing ✨

--- src/App.svelte
+++ src/App.svelte
@@ -3,25 +3,25 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello my name is {name}!</h1>
+	<p>I'm currently learning Svelte, visit my <a href="https://daooda.dev">website</a> to see what i'm doing.</p>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 250px;
 		margin: 0 auto;
 	}
-
+	a {
+		text-decoration: underline;
+	}
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
-
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
